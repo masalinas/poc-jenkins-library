3a242f795e063d51de7108c76561bb442cc39f8d
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-12-09 00:29:05 +0100

    last commit

---
+++ src/io/oferto/Kube.groovy
@@ -0,0 +1,15 @@
+package io.oferto
+
+class Kube {
+  static def docker(script) {
+    script.sh 'curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"'
+    script.sh 'chmod u+x ./kubectl'
+    script.sh './kubectl get pods'  
+  }
+
+  static def helm(script) {
+    script.sh "curl -LO https://get.helm.sh/helm-v3.16.3-linux-amd64.tar.gz"
+    script.sh "tar -zxvf helm-v3.16.3-linux-amd64.tar.gz"
+    script.sh "./linux-amd64/helm list"      
+  }
+}
