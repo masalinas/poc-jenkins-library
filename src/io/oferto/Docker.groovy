c003fc73e00d9e42046a601991d363e397f5afe6
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-12-08 21:08:42 +0100

    last commit

--- src/io/oferto/Docker.groovy
+++ src/io/oferto/Docker.groovy
@@ -3,6 +3,6 @@ package io.oferto
 
 public class Docker {
   def build(String image, String version= '1.1.0') {
-    return "Building image ${image}:${version} ..."
+    echo "Building image ${image}:${version} ..."
   }
 }
