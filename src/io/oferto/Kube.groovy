15cf9e042e812b10b06a96ede2886c9d81d7af4a
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-12-09 00:43:54 +0100

    last commit

--- src/io/oferto/Kube.groovy
+++ src/io/oferto/Kube.groovy
@@ -8,8 +8,8 @@ class Kube {
   }
 
   static def helm(script) {
-    script.sh "curl -LO https://get.helm.sh/helm-v3.16.3-linux-amd64.tar.gz"
-    script.sh "tar -zxvf helm-v3.16.3-linux-amd64.tar.gz"
-    script.sh "./linux-amd64/helm list"      
+    script.sh 'curl -LO https://get.helm.sh/helm-v3.16.3-linux-amd64.tar.gz'
+    script.sh 'tar -zxvf helm-v3.16.3-linux-amd64.tar.gz'
+    script.sh './linux-amd64/helm list'
   }
 }
