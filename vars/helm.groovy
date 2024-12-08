e01ae76574a5462ce30ad48da69dbadaaadf19f9
Miguel Salinas Gancedo <masalinas.gancedo@gmail.com>
2024-12-09 00:00:21 +0100

    last commit

---
+++ vars/helm.groovy
@@ -0,0 +1,5 @@
+static def call(script) {
+    script.sh "curl -LO https://get.helm.sh/helm-v3.16.3-linux-amd64.tar.gz"
+    script.sh "tar -zxvf helm-v3.16.3-linux-amd64.tar.gz"
+    script.sh "./linux-amd64/helm list"
+}
